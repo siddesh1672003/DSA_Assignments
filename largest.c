bfa02817de4b7fce2dcbd0c377f770188e499d49
321prasad0058 <110602321+321prasad0058@users.noreply.github.com>
2022-08-14 12:18:23 +0530

    Update largest.c

--- largest.c
+++ largest.c
@@ -2,9 +2,9 @@
 int main()
 {
     int a[5],i,max=0,n;
-    printf("Enter the size of array :");
+    printf("Enter the Array Size  :");
     scanf("%d",&n);
-    printf("Enter the array elements:");
+    printf("Enter the elements in Array");
     for(i=0;i<n;i++)
     {
         printf("a[%d]=",i);
@@ -19,7 +19,7 @@ int main()
            }
     }
 
-    printf("Largest number in the array = %d",max);
+    printf("Largest Number in the array is %d",max);
 
 
 }
